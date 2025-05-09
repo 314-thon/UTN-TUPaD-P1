b0ce2bdc3df5c18539ee8f15ce9e0231d9fbaef3
314-thon <avilas.martin@gmail.com>
2025-05-09 13:31:21 -0300

    ejercicio 4 corregido

--- 03 Estructuras Condicionales/AVILAS_TP-03.py
+++ 03 Estructuras Condicionales/AVILAS_TP-03.py
@@ -48,9 +48,8 @@ elif edad >= 12 and edad < 18:
     print("Adolescente")
 elif edad >= 18 and edad < 30:
     print("Adulto/a joven")
-elif edad >= 30:
+else:
     print("Adulto/a")
-else: pass
 
 #----------------------------------------------------------------------------------------
 
