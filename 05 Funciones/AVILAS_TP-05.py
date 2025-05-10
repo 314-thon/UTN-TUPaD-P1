8d71324b98069be178a4b4df50606cb0e4a8ab51
314-thon <avilas.martin@gmail.com>
2025-05-10 04:43:44 -0300

    me olvide de commitear, resuelto hasta el 8. Es más tranqui esta unidad.

--- 05 Funciones/AVILAS_TP-05.py
+++ 05 Funciones/AVILAS_TP-05.py
@@ -16,7 +16,8 @@ penúltimo. ¡Puedes hacerlo como se muestra en los videos o bien investigar có
 indexing con números negativos!
 """
 
-
+personajes_valorant = ["Phoenix", "Jett", "Sova", "Cypher", "Killjoy"]
+print("El penúltimo personaje de la lista es:", personajes_valorant[-2])
 
 """
 3) Crear una lista vacía, agregar tres palabras con append e imprimir la lista resultante por
@@ -25,7 +26,11 @@ ejemplo:
 lista_vacia = []
 """
 
-
+lista= []
+lista.append("pichichos")
+lista.append("michis")
+lista.append("negritos")
+print("La lista resultante es:", lista)
 
 """
 4) Reemplazar el segundo y último valor de la lista “animales” con las palabras “loro” y “oso”,
@@ -34,7 +39,10 @@ en los videos o bien investigar cómo funciona el indexing con números negativo
 animales = ["perro", "gato", "conejo", "pez"]
 """
 
-
+animales = ["perro", "gato", "conejo", "pez"]
+animales[1] = "loro"
+animales[-1] = "oso"
+print("La lista resultante es:", animales)
 
 """
 5) Analizar el siguiente programa y explicar con tus palabras qué es lo que realiza
@@ -43,13 +51,19 @@ numeros.remove(max(numeros))
 print(numeros)
 """
 
-
+#respuesta: Elimina el número mayor de la lista "numeros" y luego imprime la lista resultante sin ese número.
+#en este caso, el número mayor es 22, por lo que la lista resultante será [8, 15, 3, 7].
+#pero también funciona con floats y strings por ejemplo, pero todos los elementos de la lista deben ser del mismo tipo.
+#----------------------------------------------------------------------------------------
 
 """
 6) Crear una lista con números del 10 al 30 (incluído), haciendo saltos de 5 en 5 y mostrar por
 pantalla los dos primeros.
 """
-
+lista = []
+for i in range(10, 31, 5):
+    lista.append(i)
+print("Los dos primeros números de la lista son:", lista[:2])
 
 
 """
@@ -57,7 +71,10 @@ pantalla los dos primeros.
 cualesquiera.
 autos = ["sedan", "polo", "suran", "gol"]
 """
-
+autos = ["sedan", "polo", "suran", "gol"]
+autos[1] = "focus"
+autos[2] = "fit"
+print("La lista resultante es:", autos)
 
 
 """
@@ -65,7 +82,11 @@ autos = ["sedan", "polo", "suran", "gol"]
 directamente. Imprimir la lista resultante por pantalla.
 """
 
-
+lista = []
+lista.append(5*2)
+lista.append(10*2)
+lista.append(15*2)
+print("La lista resultante es:", lista)
 
 """
 9) Dada la lista “compras”, cuyos elementos representan los productos comprados por
